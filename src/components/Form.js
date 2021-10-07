a09949f0e469a5411b404b20f4becb008b0f9fea
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-06 23:59:25 -0600

    Show the correct alert messages on submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { AlertContext } from '../context/alert/alertContext';
 
 export const Form = () => {
+  const [value, setValue] = useState('');
+  const alert = useContext(AlertContext);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (value.trim()) {
+      //...
+      alert.show('New todo has been created', 'success');
+      // clear the input field
+      setValue('');
+    } else {
+      // shows as 'warning' by default
+      alert.show('Enter your todo');
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-group">
         <input
           type="text"
           className="form-control"
           placeholder="Enter your todo note"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
         />
       </div>
     </form>
   );
-}
+};
