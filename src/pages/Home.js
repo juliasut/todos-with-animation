1fcb942366ea25bfd9987c442632fd45967debd1
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-06 21:04:00 -0600

    Create a form component for todos input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
+import { Form } from '../components/Form';
 
 export const Home = () => {
   return (
     <Fragment>
-      <h1>Home page</h1>
+      <Form />
     </Fragment>
-  )
+  );
 };
