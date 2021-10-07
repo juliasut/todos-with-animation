a3f4e66153c3257ee2aacdf52976ab9ee703c79a
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-06 21:30:38 -0600

    Add Date to each note

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,7 +5,10 @@ export const Notes = ({ notes }) => {
     <ul className="list-group">
       {notes.map((note) => (
         <li className="list-group-item note" key={note.id}>
-          {note.title}
+          <div>
+          <strong>{note.title}</strong>
+          <small>{new Date().toLocaleDateString()}</small>
+          </div>
           <button type="button" class="btn btn-outline-danger btn-sm">&times;</button>
         </li>
       ))}
