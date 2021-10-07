4d8a45513dbdd29703a95175d4c75165036e5509
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-07 00:35:23 -0600

    Format code

--- src/context/firebase/firebaseReducer.js
+++ src/context/firebase/firebaseReducer.js
@@ -1,21 +1,22 @@
-import { ADD_NOTE, FETCH_NOTES, REMOVE_NOTE, SHOW_LOADER } from "../types"
+import { ADD_NOTE, FETCH_NOTES, REMOVE_NOTE, SHOW_LOADER } from '../types';
 
 const handlers = {
-  [SHOW_LOADER]: state => ({...state, loading: true}),
-  [ADD_NOTE]: (state, {payload}) => ({
+  [SHOW_LOADER]: (state) => ({ ...state, loading: true }),
+  // grab payload from action
+  [ADD_NOTE]: (state, { payload }) => ({
     ...state,
-    notes: [...state.notes, payload]
+    // and a new note (from payload obj) to the prev notes
+    notes: [...state.notes, payload],
   }),
-  [FETCH_NOTES]: (state, {payload}) => ({...state, notes: payload}),
-  [REMOVE_NOTE]: (state, {payload}) => ({
+  [FETCH_NOTES]: (state, { payload }) => ({ ...state, notes: payload }),
+  [REMOVE_NOTE]: (state, { payload }) => ({
     ...state,
-    notes: state.notes.filter(note => note.id !==payload)
+    notes: state.notes.filter((note) => note.id !== payload),
   }),
-  DEFAULT: state => state
-}
-
+  DEFAULT: (state) => state,
+};
 
 export const firebaseReducer = (state, action) => {
-  const handle = handlers[action.type] || handlers.DEFAULT
-  return handle(state, action)
-}
+  const handle = handlers[action.type] || handlers.DEFAULT;
+  return handle(state, action);
+};
