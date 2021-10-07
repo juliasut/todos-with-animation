0397464d5883c6a909645b0a06baae13a4de8fc7
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-07 00:31:38 -0600

    Add FirebaseState

---
+++ src/context/firebase/FirebaseState.js
@@ -0,0 +1,15 @@
+import React, { useReducer } from 'react';
+import { FirebaseContext } from './firebaseContext';
+import { firebaseReducer } from './firebaseReducer';
+
+export const FirebaseState = ({ children }) => {
+  const initialState = {
+    notes: [],
+    loading: false
+  }
+  const [state, dispatch] = useReducer(firebaseReducer, initialState);
+
+  return <FirebaseContext.Provider value={{
+    
+  }}>{children}</FirebaseContext.Provider>;
+};
