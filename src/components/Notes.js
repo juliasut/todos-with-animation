4939b1166a11849f63dd53bb509138bdd4f68298
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-08 03:04:09 -0600

    Add onRemove to a delete button

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-export const Notes = ({ notes }) => {
+export const Notes = ({ notes, onRemove }) => {
   return (
     <ul className="list-group">
       {notes.map((note) => (
         <li className="list-group-item note" key={note.id}>
           <div>
-          <strong>{note.title}</strong>
-          <small>{new Date().toLocaleDateString()}</small>
+            <strong>{note.title}</strong>
+            <small>{new Date().toLocaleDateString()}</small>
           </div>
-          <button type="button" class="btn btn-outline-danger btn-sm">&times;</button>
+          <button
+            type="button"
+            className="btn btn-outline-danger btn-sm"
+            onClick={() => onRemove(note.id)}
+          >
+            &times;
+          </button>
         </li>
       ))}
     </ul>
