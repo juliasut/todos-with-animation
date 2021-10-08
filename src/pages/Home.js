80c42e3b0da732f1b9a1945fb641198c46f2b7f1
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-08 02:32:21 -0600

    Render loader if data isn't available

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import { Form } from '../components/Form';
+import { Loader } from '../components/Loader';
 import { Notes } from '../components/Notes';
 import { FirebaseContext } from '../context/firebase/firebaseContext';
 
@@ -15,7 +16,8 @@ export const Home = () => {
       <Form />
 
       <hr />
-      <Notes notes={notes} />
+
+      {loading ? <Loader /> : <Notes notes={notes} />}
     </Fragment>
   );
 };
