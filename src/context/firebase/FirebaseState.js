6114386527e04c13f2605a4a7228689b994f20da
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-08 02:24:36 -0600

    Add values for FirebaseContext

--- src/context/firebase/FirebaseState.js
+++ src/context/firebase/FirebaseState.js
@@ -1,15 +1,58 @@
 import React, { useReducer } from 'react';
 import { FirebaseContext } from './firebaseContext';
+import axios from 'axios';
 import { firebaseReducer } from './firebaseReducer';
+import { REMOVE_NOTE, SHOW_LOADER } from '../types';
+
+const url = process.env.REACT_APP_DB_URL;
 
 export const FirebaseState = ({ children }) => {
   const initialState = {
     notes: [],
-    loading: false
-  }
+    loading: false,
+  };
   const [state, dispatch] = useReducer(firebaseReducer, initialState);
 
-  return <FirebaseContext.Provider value={{
-    
-  }}>{children}</FirebaseContext.Provider>;
+  const showLoader = () => dispatch({ type: SHOW_LOADER });
+  const fetchNotes = async () => {
+    showLoader();
+    const res = await axios.get(`${url}/notes.json`);
+
+    console.log('fetchNotes', res.data);
+  };
+
+  const addNote = async (title) => {
+    const note = {
+      title,
+      date: new Date().toJSON(),
+    };
+
+    const res = await axios.post(`${url}/notes.json`, note);
+
+    console.log('addNote', res.data);
+  };
+
+  const removeNote = async (id) => {
+    await axios.delete(`${url}/notes/${id}.json`);
+
+    dispatch({
+      type: REMOVE_NOTE,
+      payload: id,
+    });
+  };
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        showLoader,
+        addNote,
+        removeNote,
+        fetchNotes,
+        loading: state.loading,
+        notes: state.notes,
+      }}
+    >
+      {children}
+    </FirebaseContext.Provider>
+  );
 };
